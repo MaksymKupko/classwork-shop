17f232c6cc77630c8d2b4d81f9fe28a76d06b666
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-13 20:57:33 +0200

    added CARDS_SERVICE_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,5 @@ export const createConfig = () => {
   config();
   EnvConfig.PORT = process.env.PORT || 3000;
   EnvConfig.SECRET_KEY = process.env.SECRET_KEY;
+  EnvConfig.CARDS_SERVICE_URL = process.env.CARDS_SERVICE_URL;
 };
