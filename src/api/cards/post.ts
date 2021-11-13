008eadce830ba7ae5d79bfbbf9b342ea3927ae79
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-13 23:30:02 +0200

    finished post card request

--- src/api/cards/post.ts
+++ src/api/cards/post.ts
@@ -1,14 +1,24 @@
 import { Response } from "express";
 import { assign, pick } from "lodash";
 import { CardEntity } from "../../db/entities/card.entity";
+import CardsService, { TGetCard } from "../../services/cards.service";
 import { IRequest } from "../../tools/types";
-import { wrapper } from "../../tools/wrapper.helpers";
+import { HttpError, wrapper } from "../../tools/wrapper.helpers";
 
 export const postCards = wrapper(async (req: IRequest, res: Response) => {
-  const data = pick(req.body, "number", "expired", "cvv");
-  const card = new CardEntity();
-  assign(card, data);
-  // await card.save();
+  const data: TGetCard = pick(req.body, "number", "expired", "cvv");
 
-  res.sendStatus(200);
+  if (await CardEntity.findOne({ where: { number: data.number } })) {
+    throw new HttpError("This card had been already added");
+  }
+
+  const cardsService = new CardsService();
+  const card = await cardsService.getCard(data);
+
+  const entity = new CardEntity();
+  assign(entity, card);
+  entity.userId = req.user.id;
+  await entity.save();
+
+  res.status(200).send(entity);
 });
