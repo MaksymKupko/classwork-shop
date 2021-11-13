74ec5b6120df08cfcd0b22eca20542d6ac487696
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-13 14:23:38 +0200

    Heroku

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { UserEntity } from "../../db/entities/user.entity";
 import { validationMiddleware } from "../../tools/validate-body.middleware";
 import { registration, login } from "./post";
+import { RegistrationRequest } from "./requests/registration.request";
 
 const router = Router();
-const userValidationMiddleware = validationMiddleware(UserEntity);
 
-router.post("/registration", userValidationMiddleware, registration);
+router.post("/registration", validationMiddleware(RegistrationRequest), registration);
 router.post("/login", login);
 
 export default router;
