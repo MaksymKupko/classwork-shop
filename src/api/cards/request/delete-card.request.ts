2aa0ff0b24059350d3fe2e7379840f2d27be4fff
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-14 18:27:27 +0200

    deleted dto

--- src/api/cards/request/delete-card.request.ts
+++
@@ -1,7 +0,0 @@
-import { IsCreditCard } from "class-validator";
-import { BaseRequest } from "../../common/base.request";
-
-export class DeleteCardRequest extends BaseRequest {
-  @IsCreditCard()
-  number: string;
-}
