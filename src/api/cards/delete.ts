27baf639860afe36a6a48bda1c99d2ff6e6e247a
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-14 18:27:16 +0200

    changed deletecard

--- src/api/cards/delete.ts
+++ src/api/cards/delete.ts
@@ -1,18 +1,17 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { CardEntity } from "../../db/entities/card.entity";
-import { IRequest } from "../../tools/types";
+import { IEntityRequest } from "../../tools/types";
 import { HttpError, wrapper } from "../../tools/wrapper.helpers";
 
-export const deleteCards = wrapper(async (req: IRequest, res: Response) => {
+export const deleteCards = wrapper(async (req: IEntityRequest<CardEntity>, res: Response) => {
   const userId = req.user.id;
-  const number = req.params.number;
-  const card = await CardEntity.findOne({ where: { number, userId } });
+  const card = req.entity;
 
-  if (!card) {
+  if (card.userId !== userId) {
     throw new HttpError("You don't have such a card");
   }
 
-  await card.softRemove();
+  await card.remove();
 
   return res.sendStatus(200);
 });
--- src/api/cards/index.ts
+++ src/api/cards/index.ts
@@ -7,7 +7,6 @@ import { getCards, getOneCard } from "./get";
 import { patchCards } from "./patch";
 import { postCards } from "./post";
 import { putCards } from "./put";
-import { DeleteCardRequest } from "./request/delete-card.request";
 import { PostCardRequest } from "./request/post-card.request";
 
 const router = Router();
@@ -16,7 +15,7 @@ router.get("/", getCards);
 router.get("/:id", findItemByIdMiddleware(CardEntity), getOneCard);
 router.post("/", validationMiddleware(PostCardRequest), postCards);
 router.put("/", putCards);
-router.delete("/:number", validationMiddleware(DeleteCardRequest, "params"), deleteCards);
+router.delete("/:id", findItemByIdMiddleware(CardEntity), deleteCards);
 router.patch("/", patchCards);
 
 export default router;
