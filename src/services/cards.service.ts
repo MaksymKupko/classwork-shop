7bc0946e17715dbbc731942061d7aae584854c97
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-14 20:40:46 +0200

    refactored card service

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -1,17 +1,12 @@
 import axios, { AxiosInstance } from "axios";
 import cardTypeLib from "credit-card-type";
 import { EnvConfig } from "../config";
+import { CardEntity } from "../db/entities/card.entity";
+import { CardTypeEnum } from "../enums/card-type.enum";
 
-export type TCard = {
-  number: string;
-  cvv: number;
-  expired: string;
-  balance: number;
-  type: string;
-};
-
-export type TCardParams = Omit<TCard, "type" | "balance">;
-
+export type TCardTransactionParams = Pick<CardEntity, "number" | "cvv" | "expired">;
+export type TCardTransactionResponse = { balance: number };
+export type TCardReturn = TCardTransactionParams & TCardTransactionResponse & { type: string };
 export class CardsService {
   api: AxiosInstance;
 
@@ -19,16 +14,16 @@ export class CardsService {
     this.api = axios.create({ baseURL: EnvConfig.CARDS_SERVICE_URL });
   }
 
-  public async getCard(card: TCardParams): Promise<TCard> {
+  public async getCard(cardParams: TCardTransactionParams): Promise<TCardReturn> {
     try {
-      const response = await this.api.get<Pick<TCard, "balance">>("/balance", {
-        params: card,
+      const response = await this.api.get<TCardTransactionResponse>("/balance", {
+        params: cardParams,
       });
       const balance = response.data.balance;
-      const type = this.getCardType(card.number);
+      const type = this.getCardType(cardParams.number) as CardTypeEnum;
 
       return {
-        ...card,
+        ...cardParams,
         balance,
         type,
       };
@@ -37,21 +32,22 @@ export class CardsService {
     }
   }
 
-  public async withdrawFromCard(card: TCardParams, sum: number): Promise<boolean> {
+  public async withdrawFromCard(cardParams: TCardTransactionParams, sum: number): Promise<boolean> {
     try {
-      await this.api.patch<Pick<TCard, "balance">>("/withdraw", { sum }, { params: card });
+      await this.api.patch<TCardTransactionResponse>("/withdraw", { sum }, { params: cardParams });
       return true;
     } catch (error) {
-      throw error;
+      throw new Error("Something went wrong during withdrawal from card.");
     }
   }
 
-  public async depositToCard(number: Pick<TCard, "number">): Promise<boolean> {
+  public async depositToCard(cardNumber: string, sum: number): Promise<boolean> {
     try {
-      await this.api.patch<typeof number>("/deposit", {}, { params: number });
+      await this.api.patch<TCardTransactionResponse>("/deposit", { sum }, { params: { number: cardNumber } });
       return true;
     } catch (error) {
-      throw error;
+      console.log(error);
+      throw new Error("Something went wrong during deposit to card");
     }
   }
 
