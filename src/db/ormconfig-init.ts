40c368faa125fbff3bba101537b64670695f2beb
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-16 20:57:21 +0200

    Heroku

---
+++ src/db/ormconfig-init.ts
@@ -0,0 +1,22 @@
+import { config } from "dotenv";
+import { writeFile } from "fs/promises";
+import path from "path";
+
+const initOrmConfig = async () => {
+  config();
+
+  const env = process.env.ENV;
+  const dir = env === "DEV" ? "src" : "dist/src";
+
+  const opt = {
+    type: "postgres",
+    url: process.env.POSTGRES_URL,
+    entities: [`${dir}/**/entities/*.entity{.ts, .js}`],
+    migrations: [`${dir}/**/migrations/*.entity{.ts, .js}`],
+    migrationsDir: `${dir}/**/migrations`,
+  };
+  console.log(path.join(__dirname, "../../"));
+  await writeFile(path.join(__dirname, "../../ormconfig.json"), JSON.stringify(opt, null, 4));
+};
+
+initOrmConfig().then(() => console.log("ormconfig.json have been generated"));
