a44d6cdfee18ae23938a6f88bbeb1ff540df57d3
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-17 15:43:47 +0200

    patchPurachaseRequest fix

--- src/api/purchases/requests/patch-purchase.request.ts
+++ src/api/purchases/requests/patch-purchase.request.ts
@@ -1,7 +1,8 @@
 import { IsEnum } from "class-validator";
 import { PurchaseStatusEnum } from "../../../enums/purchase-status.enum";
+import { BaseRequest } from "../../common/base.request";
 
-export class PatchPurchaseRequest {
+export class PatchPurchaseRequest extends BaseRequest {
   @IsEnum(PurchaseStatusEnum)
   status: PurchaseStatusEnum;
 }
