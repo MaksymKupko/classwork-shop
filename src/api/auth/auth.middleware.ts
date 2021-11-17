a06c5ff9e5f191e047d30ed95cabc17b2d117f7d
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-17 12:58:05 +0200

    auth middlewares wrapper

--- src/api/auth/auth.middleware.ts
+++ src/api/auth/auth.middleware.ts
@@ -3,8 +3,9 @@ import { UserEntity } from "../../db/entities/user.entity";
 import { UserRoleEnum } from "../../enums/user-role.enum";
 import JwtService from "../../services/jwt.service";
 import { IRequest } from "../../tools/types";
+import { HttpError, wrapper } from "../../tools/wrapper.helpers";
 
-export const authMiddleware = async (req: IRequest, res: Response, next) => {
+export const authMiddleware = wrapper(async (req: IRequest, res: Response, next) => {
   const token = (req.headers.authorization || "").split(" ")[1];
 
   try {
@@ -13,19 +14,16 @@ export const authMiddleware = async (req: IRequest, res: Response, next) => {
     req.user = user;
     next();
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    throw new HttpError("Invalid Token", 401);
   }
-};
+});
 
-export const authByRoleMiddleware = (role: UserRoleEnum) => {
-  return async (req: IRequest, res: Response, next) => {
+export const authByRoleMiddleware = (role: UserRoleEnum) =>
+  wrapper(async (req: IRequest, res: Response, next) => {
     const user = req.user;
 
     if (user.role !== role) {
-      // throw new HttpError(`This action is not permitted for role ${user.role}`, 401);
-      //TODO wrapperMiddle
-      res.status(401).send(`This action is not permitted for role ${user.role}`);
+      throw new HttpError(`This action is not permitted for role ${user.role}`, 401);
     }
     next();
-  };
-};
+  });
