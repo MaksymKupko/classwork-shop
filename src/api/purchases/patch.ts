95157f0368954833afa4f13136b6e350968964fe
Maksym Kupko <maksym.kupko11@gmail.com>
2021-11-18 20:19:44 +0200

    deleted unused code (because of wrapper)

--- src/api/purchases/patch.ts
+++ src/api/purchases/patch.ts
@@ -7,18 +7,6 @@ import { HttpError, middlewareWrapper, wrapper } from "../../tools/wrapper.helpe
 export const patchPurchases = wrapper(async (req: IEntityRequest<PurchaseEntity>, res: Response) => {
   const { status } = req.body;
 
-  if (!status) {
-    throw new HttpError("Please, provide status to change");
-  }
-
-  const availableStatuses = Object.values(PurchaseStatusEnum);
-
-  if (!availableStatuses.includes(status)) {
-    throw new HttpError(
-      `Please, provide valid status to change. Status have to be one of the next: ${availableStatuses.join(", ")}`
-    );
-  }
-
   const purchase = req.entity;
 
   if (purchase.customerId !== req.user.id) {
