5099f4d6d0740c51c55c8282932c6686a5c89dbc
Maksym Kupko <maksym.kupko11@gmail.com>
2021-12-01 12:28:09 +0200

    fixed getItemById for seller

--- src/api/sellers/get.ts
+++ src/api/sellers/get.ts
@@ -1,8 +1,20 @@
 import { Response } from "express";
-import { IRequest } from "../../tools/types";
-import { wrapper } from "../../tools/wrapper.helpers";
+import { ItemEntity } from "../../db/entities/item.entity";
+import { IEntityRequest, IRequest } from "../../tools/types";
+import { HttpError, wrapper } from "../../tools/wrapper.helpers";
 
 export const getItems = wrapper(async (req: IRequest, res: Response) => {
   const items = await req.user.items;
   return res.status(200).send(items);
+});
+
+export const getItemById = wrapper(async (req: IEntityRequest<ItemEntity>, res: Response) => {
+  const item = req.entity;
+  const user = req.user;
+
+  if (item.sellerId !== user.id) {
+    throw new HttpError("Permission denied");
+  }
+
+  res.status(200).send(item);
 });
